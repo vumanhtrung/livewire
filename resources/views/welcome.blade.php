51e6155a760b51579d8f1c182cec5eb6cb9a6a40
Vu Manh Trung <manhtrungit8x@gmail.com>
2022-09-20 12:34:55 +0700

    Add LocationController

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
             <div class="flex flex-col sm:justify-center items-center pt-5 pb-5">
                 <h2 class="font-bold text-2xl">Livewire Test</h2>
                 <div class="w-full sm:max-w-3xl mt-6 mb-6 px-6 py-8 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                    <livewire:location />
+                    @yield('content')
                 </div>
             </div>
         </div>
